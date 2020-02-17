3d025aeace418b8e7966d3c8c777919afdaa90ee
crhisctian <crhisclick@gmail.com>
2020-02-17 16:17:49 -0400

    practica de rutas de laravel

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,32 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function(){
+    return 'home';
+});
+
+
+Route::get('/usuarios' , function(){
+    return 'usuarios';
+});
+
+
+Route::get('/usuarios/{id}', function($id){
+    return "mostrando detalle del id {$id} ";
+})->where('id','[0-9]+');
+Route::get('/usuarios/nuevo', function($id){
+    return "crear nuevo usuario";
+});
+Route::get('/saludo/{name}/{nickname?}', function($name,$nickname=NULL){
+
+    if($nickname){
+       return "Bienvenido {$name}, alias {$nickname}";
+    }else{
+        return "Bienvenido {$name}";
+    }
+
 });
